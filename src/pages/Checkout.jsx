94a44c39f9b65327743c4d94eceb8a86b8ceb967
Matthew <mattgranato2004@gmail.com>
2026-01-04 22:12:52 -0500

    Fix payment button to show correct currency (USD/CAD) instead of hardcoded CAD

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -943,7 +943,7 @@ export default function Checkout() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                           </svg>
                           {hasEnteredShippingDetails && selectedShipping 
-                            ? `${getTranslation(language, 'checkout.paySecurely')} ${total.toFixed(2)} ${getTranslation(language, 'checkout.paySecurelySuffix')}`
+                            ? `${getTranslation(language, 'checkout.paySecurely')} ${formatPrice(total)} ${currency}`
                             : (language === 'fr' ? 'Entrez les détails d\'expédition' : 'Enter shipping details')
                           }
                         </>
