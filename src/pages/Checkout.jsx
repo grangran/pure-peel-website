3bc2592778cc37e92917e7166d1a4574751e7ed0
Matthew <mattgranato2004@gmail.com>
2026-01-05 23:31:35 -0500

    Remove unused useRef import

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { useCart } from "../context/CartContext"
 import { useScrollReveal } from "../hooks/useScrollReveal"
 import { useLanguage } from "../context/LanguageContext"
